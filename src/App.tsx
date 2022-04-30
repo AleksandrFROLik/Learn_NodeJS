3465ab1d3396c4ef5854dee8d31ea3f047a64d15
AleksandrFROLik <nextfrolik@gmail.com>
2022-04-30 21:36:31 +0300

    start redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,49 @@
 import React, {useState} from 'react';
 import './App.css';
-import {TodoList} from "./TodoList";
-import {v1} from "uuid";
-
-export type propsFilteredTasks = 'All' | 'Active' | 'Completed'
-
-function App() {
-    let [tasks, setTasks] = useState([
-        {id: v1(), title: "HTML&CSS", isDone: true},
-        {id: v1(), title: "JavaScript", isDone: true},
-        {id: v1(), title: "NodeJS", isDone: false},
-        {id: v1(), title: "Rest API", isDone: false},
-        {id: v1(), title: "GraphQL", isDone: false},
-    ])
+import {TodoList} from "./components/todoList/TodoList";
+import {InputField} from "./components/inputField/InputField";
 
 
-    const changeStatus = (isDone: boolean, tID: string) => {
-        setTasks(tasks.map(m => m.id === tID ? ({...m, isDone: isDone}) : m))
-    }
-    const removeTasks = (id: string) => {
-        let deleteTask = tasks.filter(f => f.id !== id)
-        setTasks(deleteTask)
-    }
-    const addTask = (newTitle: string) => {
-        if (newTitle.trim() !== '') {
-            let task = {id: v1(), title: newTitle.trim(), isDone: false}
-            let newTasks = [task, ...tasks]
-            setTasks(newTasks)
+export type TodosType = {
+    id: string
+    text: string
+    completed: boolean
+}
+
+export const App = () => {
+
+    const [todos, setTodos] = useState<TodosType[]>([])
+    const [text, setText] = useState('')
+
+    const addText = () => {
+        if (text.trim().length) {
+            setTodos([
+                ...todos,
+                {
+                    id: new Date().toISOString(),
+                    text,
+                    completed: false
+                }
+            ])
+            setText('')
         }
-    }
+    };
 
-    let [filter, setFilter] = useState<propsFilteredTasks>('All')
-    let filterForTodoList = tasks
-    if (filter === 'Active') {
-        filterForTodoList = tasks.filter(f => !f.isDone)
-    }
-    if (filter === 'Completed') {
-        filterForTodoList = tasks.filter(f => f.isDone)
-    }
 
-    const changeFilter = (value: propsFilteredTasks) => {
-        setFilter(value)
-    }
+    const removeTodo = (todoId: string) => {
+        setTodos(todos.filter(todo => todo.id !== todoId))
+    };
+
+    const handleChecked = (todoId: string) => {
+        setTodos(todos.map(todo => todo.id === todoId ? {...todo, completed: !todo.completed} : todo))
+    };
 
     return (
         <div className="App">
-            <TodoList title='What to learn'
-                      tasks={filterForTodoList}
-                      removeTasks={removeTasks}
-                      changeFilter={changeFilter}
-                      addTask={addTask}
-                      changeStatus={changeStatus}
-                      filter={filter}
-            />
+            <InputField text={text} setText={setText} addText={addText}/>
+            <TodoList todos={todos} handleChecked={handleChecked} removeTodo={removeTodo}/>
         </div>
     );
 }
 
-export default App;
+
